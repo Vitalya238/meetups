a6376822c743dc2f1ceae625312ddee26da7f417
BOSS <vitalya@DivineCreation.local>
2024-06-18 16:09:27 +0300

    index modified

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const app = require("./express");
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
+app.listen(PORT, () => console.log(`Aboba ${PORT}`));
