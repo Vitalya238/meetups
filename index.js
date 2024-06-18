0ef9713bc49bfe68ec66729d8c59a86affe202c6
BOSS <vitalya@DivineCreation.local>
2024-06-18 16:33:59 +0300

    asdsa

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
-const app = require("express");
-=======
+
 const app = require("express")();
->>>>>>> d6318d140d00462d36b468b809f43b80efee8e50
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Server listening on ${PORT}`));
